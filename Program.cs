7f2cc3004fc5006db5ad3d097d4ac8730d889181
Todd Smitala <tc.smitala@gmail.com>
2019-10-24 16:03:04 -0400

    Add message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace VisualStudioDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Visual Studio Demo");
+            Console.ReadLine();
         }
     }
 }
